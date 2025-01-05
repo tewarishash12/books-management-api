f815babfd830347f4ae28aee5a4c469dcc86243e
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-05 22:28:08 +0530

    Created schema for author

--- models/author.js
+++ models/author.js
@@ -4,9 +4,7 @@ const AuthorSchema = new mongoose.Schema({
     name: {type: String, required:true},
     dob: {type: Date, required:true},
     nationality: {type: String, required:true},
-    Books: {type: [mongoose.Schema.Types.ObjectId], ref:"Books", default:[], required:true}
+    books: {type: [mongoose.Schema.Types.ObjectId], ref:"Books", default:[], required:true}
 })
 
 module.exports = mongoose.model("Author",AuthorSchema);
-
-// author has a name, date of birth, nationality, and a list of books they have
